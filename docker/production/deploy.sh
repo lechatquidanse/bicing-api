184d2281ad3f774aa23ee357d8c3d6d01f9d919b
lechatquidanse <stephane.elmanouni@gmail.com>
2018-11-23 14:52:11 +0000

    [Release] - 1.5.1

--- docker/production/deploy.sh
+++ docker/production/deploy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # Prepare environment file and docker-compose
-envsubst < ./docker/stages/production/.env.dist > ./docker/stages/production/.env
-envsubst < ./docker/stages/production/docker-compose.yml.dist > ./docker/stages/production/docker-compose.yml
+envsubst < ./docker/production/.env.dist > ./docker/production/.env
+envsubst < ./docker/docker-compose.production.yml > ./docker/docker-compose.yml
 
 # Prepare ssh connection with production server
 eval $(ssh-agent -s)
@@ -11,8 +11,9 @@ mkdir -p ~/.ssh
 chmod 700 ~/.ssh
 
 # Copy environment file and docker-compose to production server
-scp -o 'StrictHostKeyChecking no' ./docker/stages/production/.env ${SERVER_PRODUCTION}:/var/www/bicing-api/
-scp -o 'StrictHostKeyChecking no' ./docker/stages/production/docker-compose.yml ${SERVER_PRODUCTION}:/var/www/bicing-api/
+scp -o 'StrictHostKeyChecking no' ./docker/production/.env ${SERVER_PRODUCTION}:/var/www/bicing-api/
+scp -o 'StrictHostKeyChecking no' ./docker/docker-compose.yml ${SERVER_PRODUCTION}:/var/www/bicing-api/
+
 
 # Run commands to update docker containers and run migrations
 ssh -o "StrictHostKeyChecking no" ${SERVER_PRODUCTION} "docker login -u ${REGISTRY_PRODUCTION_LOGIN} -p ${REGISTRY_PRODUCTION_PASSOWRD} registry.gitlab.com"
@@ -23,3 +24,5 @@ ssh -o "StrictHostKeyChecking no" ${SERVER_PRODUCTION} "docker logout registry.g
 
 # Clean
 rm -rf ~/.ssh
+rm ./docker/production/.env
+rm ./docker/docker-compose.yml
