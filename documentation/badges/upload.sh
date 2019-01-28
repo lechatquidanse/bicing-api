bc390b7b0199d9d000d091abdc1a3d125b27dff3
lechatquidanse <stephane.elmanouni@gmail.com>
2019-01-28 10:57:57 +0100

    add private token to get badges.

--- documentation/badges/upload.sh
+++ documentation/badges/upload.sh
@@ -54,7 +54,7 @@ PAYLOAD=$(cat << JSON
 JSON
 )
 
-curl --request POST \
-    --header "PRIVATE-TOKEN: ${BADGE_REPOSITORY_PRIVATE_TOKEN}" \
-    --header "Content-Type: application/json" \
-    --data "$PAYLOAD" https://gitlab.com/api/v4/projects/10513975/repository/commits
+#curl --request POST \
+#    --header "PRIVATE-TOKEN: ${BADGE_REPOSITORY_PRIVATE_TOKEN}" \
+#    --header "Content-Type: application/json" \
+#    --data "$PAYLOAD" https://gitlab.com/api/v4/projects/10513975/repository/commits
